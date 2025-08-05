cd45dee7749f761635b123b9fbaf4ecdd8e90a94
sally <sally@gmail.com>
2025-08-05 12:01:44 +0300

    add ex2

---
+++ easy-ex2.py
@@ -0,0 +1,26 @@
+#Ask the user for a number. Depending on whether the number is even or odd, print out an 
+# appropriate message to the user. 
+# Hint: how does an even / odd number react differently when divided by 2?
+
+#Extras:
+
+#If the number is a multiple of 4, print out a different message.
+#  Ask the user for two numbers: one number to check (call it num) and one number to divide by 
+# (check). If check divides evenly into num, tell that to the user. If not, print a different 
+# appropriate message.
+a=int(input("please enter a number "))
+if(a % 2 == 0)and not(a % 4 == 0):
+    print(a , " is even ")
+elif (a % 4 == 0):
+    print(a , " is multiple of 4  ")
+else:
+    print(a," is odd ") 
+num=int(input("please enter  number 1 :"))
+check=int(input("please enter  number 2 :"))
+result=num%check
+if(result % 2 == 0):
+    print(num, "divided by",check ," is even ") 
+else:
+    print(num,"divided by",check ," is odd ") 
+
+
