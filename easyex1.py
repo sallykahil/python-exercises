d0eedf515efe7518c4acf29995c1e1acd23626f1
sally <sally@gmail.com>
2025-08-05 11:02:40 +0300

    add ex1

---
+++ easyex1.py
@@ -0,0 +1,13 @@
+#Write a python program that:
+
+#Asks the user about their name
+#Save it in a variable called "name"
+#Asks the user about their age
+#Save it in a variable called "age"
+#Print a string stating "The user -name- is -age- years old"
+
+name=""
+age=0
+name=input("whats your name ? ")
+age=input("whats your age ? ")
+print("The user "+name+" is "+age+" years old")
